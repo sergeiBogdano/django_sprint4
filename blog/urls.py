bf459880e8997e9a5348d7b0c255b420d7e1f147
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2024-11-28 14:11:45 +0400

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
-
-from . import views
+from .views import (
+    index, post_detail, create_post, edit_post,
+    delete_post, category_posts, add_comment,
+    edit_comment, registration, profile, edit_profile,
+    user_posts_view, delete_comment
+)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
-    path('posts/create/', views.create_post, name='create_post'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:id>/edit/', views.edit_post, name='edit_post'),
-    path('posts/<int:id>/delete/', views.delete_post, name='delete_post'),
-    path('posts/<int:post_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path('posts/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
-    path('posts/<int:post_id>/edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('', index, name='index'),
+    path('posts/<int:id>/', post_detail, name='post_detail'),
+    path('create/', create_post, name='create_post'),
+    path('posts/<int:id>/edit/', edit_post, name='edit_post'),
+    path('posts/<int:id>/delete/', delete_post, name='delete_post'),
+    path('category/<slug:category_slug>/', category_posts, name='category_posts'),
+    path('posts/<int:post_id>/comment/', add_comment, name='add_comment'),
+    path('posts/<int:post_id>/edit_comment/<int:comment_id>/', edit_comment, name='edit_comment'),
+    path('registration/', registration, name='registration'),
+    path('profile/<str:username>/', profile, name='profile'),
+    path('edit_profile/', edit_profile, name='edit_profile'),
+    path('user_posts/', user_posts_view, name='user_posts'),
+    path('posts/<int:post_id>/delete_comment/<int:comment_id>/', delete_comment, name='delete_comment'),
 ]
