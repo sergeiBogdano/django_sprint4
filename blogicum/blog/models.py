7a90e68fea52196b75874c50722f0147a5f0e7d4
sergeiBogdano <sergeibigd@gmail.com>
2024-12-12 14:33:42 +0400

    d

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,7 +6,7 @@ from .constants import MAX_FIELD_LENGTH, MAX_SHORT_STRING_LENGTH
 User = get_user_model()
 
 
-class BaseModel(models.Model):
+class PublishedModel(models.Model):
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
@@ -20,7 +20,7 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Category(BaseModel):
+class Category(PublishedModel):
     title = models.CharField(
         max_length=MAX_FIELD_LENGTH,
         verbose_name='Заголовок'
@@ -36,13 +36,13 @@ class Category(BaseModel):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
-        ordering = ['title']
+        ordering = ('title',)
 
     def __str__(self):
         return self.title[:MAX_SHORT_STRING_LENGTH]
 
 
-class Location(BaseModel):
+class Location(PublishedModel):
     name = models.CharField(
         max_length=MAX_FIELD_LENGTH,
         verbose_name='Название места'
@@ -51,12 +51,13 @@ class Location(BaseModel):
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
+        ordering = ('name',)
 
     def __str__(self):
         return self.name[:MAX_SHORT_STRING_LENGTH]
 
 
-class Post(BaseModel):
+class Post(PublishedModel):
     title = models.CharField(
         max_length=MAX_FIELD_LENGTH,
         verbose_name='Заголовок'
@@ -67,20 +68,26 @@ class Post(BaseModel):
         help_text='Если установить дату и время в будущем — можно делать '
                   'отложенные публикации.'
     )
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               verbose_name='Автор публикации',
-                               related_name='posts')
-    location = models.ForeignKey(Location,
-                                 on_delete=models.SET_NULL,
-                                 verbose_name='Местоположение',
-                                 null=True,
-                                 related_name='posts')
-    category = models.ForeignKey(Category,
-                                 on_delete=models.SET_NULL,
-                                 null=True,
-                                 verbose_name='Категория',
-                                 related_name='posts')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
+        related_name='posts'
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        verbose_name='Местоположение',
+        null=True,
+        related_name='posts'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория',
+        related_name='posts'
+    )
     image = models.ImageField(
         upload_to='posts/',
         verbose_name='Изображение',
@@ -107,7 +114,8 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор'
+        verbose_name='Автор',
+        related_name='comments'
     )
     text = models.TextField(verbose_name='Текст комментария')
     created_at = models.DateTimeField(
